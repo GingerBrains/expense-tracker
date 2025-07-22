d30581274b30914d9094599b2f5a4eac76756899
laurelld <73134011+GingerBrains@users.noreply.github.com>
2025-07-23 00:00:29 +0100

    Added api paths for fetching dashboard data

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -5,23 +5,26 @@ export const API_PATHS = {
     AUTH: {
         LOGIN: "/api/v1/auth/login",
         REGISTER: "/api/v1/auth/register",
-        GET_USER: "/api/v1/auth/getUser",
+        GET_USER_INFO: "/api/v1/auth/getUser",
         UPDATE: "/api/v1/auth/update",
-        FORGOT_PASSWORD: "/api/v1/auth/forgot-password",
-        RESET_PASSWORD: "/api/v1/auth/reset-password",
-        VERIFY_EMAIL: "/api/v1/auth/verify-email",
     },
-    EXPENSE: {
-        ADD: "/api/v1/expense/add",
-        LIST: "/api/v1/expense/list",
-        DELETE: "/api/v1/expense/delete",
-        UPDATE: "/api/v1/expense/update",
+    DASHBOARD: {
+        GET_DATA: "/api/v1/dashboard",
     },
     INCOME: {
-        ADD: "/api/v1/income/add",
-        LIST: "/api/v1/income/list",
-        DELETE: "/api/v1/income/delete",
-        UPDATE: "/api/v1/income/update",
+        ADD_INCOME: "/api/v1/income/add",
+        GET_ALL_INCOME: "/api/v1/income/get",
+        DELETE_INCOME: (incomeId) => `/api/v1/income/${incomeId}`,
+        DOWNLOAD_INCOME: `/api/v1/income/downloadexcel`,
+    },
+    EXPENSE: {
+        ADD_EXPENSE: "/api/v1/expense/add",
+        GET_ALL_EXPENSE: "/api/v1/expense/get",
+        DELETE_EXPENSE: (expenseId) => `/api/v1/expense/${expenseId}`,
+        DOWNLOAD_EXPENSE: `/api/v1/expense/downloadexcel`,
+    },
+    IMAGE: {
+        UPLOAD_IMAGE: "/api/v1/auth/upload-image",
     },
 };
         
