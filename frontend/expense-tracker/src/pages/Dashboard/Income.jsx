d2ae065e7696a082f68893bdeef34c3eef4bd521
laurelld <73134011+GingerBrains@users.noreply.github.com>
2025-07-22 23:46:16 +0100

    Fixed add income api endpoint

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -64,7 +64,7 @@ const Income = () => {
       }
 
       try {
-        await axiosInstance.post(API_PATHS.INCOME.ADD_INCOME, {
+        await axiosInstance.post(API_PATHS.INCOME.ADD, {
           source,
           amount,
           date,
